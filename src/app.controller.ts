c2a68c4e7adf7a9d25d6887d35c4a2c21062c109
Torr7s <torresmiranda.rj@gmail.com>
2022-04-06 15:07:52 -0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+
 import { Request, Response } from 'express';
 
 import { AppService } from './app.service';
 
+@ApiTags('Articles')
 @Controller('/')
 export class AppController {
   constructor(private readonly _appService: AppService) { }
+
+  @ApiResponse({ status: 200, description: 'Back-end Challenge 2021 🏅 - Space Flight News' })
 
   @Get()
   async handle(@Req() request: Request, @Res() response: Response): Promise<Response> {
