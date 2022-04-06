cc3614be074cd79857d2de483979ef388ffc2e43
Torr7s <torresmiranda.rj@gmail.com>
2022-04-06 15:07:30 -0300

    Update main.ts
    Documentation Setup with Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Swagger Documentation - Coodesh Challenge')
+    .setDescription(
+      'This documentation is intended to show all the functioning of this application, which was developed for a Coodesh challenge.'
+    )
+    .setVersion('3.4.0')
+    .addTag('Articles')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3030)
 }
 
