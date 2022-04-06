d44d9d92f49598ea7d32d80de0594dd87cad04de
Torr7s <torresmiranda.rj@gmail.com>
2022-04-06 15:08:13 -0300

    Update create-articles.service.ts

--- src/modules/articles/useCases/createArticle/create-articles.service.ts
+++ src/modules/articles/useCases/createArticle/create-articles.service.ts
@@ -3,7 +3,6 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateArticleDto } from '@modules/articles/domain/dtos/create-article.dto';
 
 import { ArticleModel } from '@modules/articles/infra/mongoose/models/article.model';
-
 import { ArticlesRepository } from '@modules/articles/infra/repositories/articles.repository';
 
 @Injectable()
